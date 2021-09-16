b9cb46a7f3f0c82c35e61e53b400b3a3c48fbfe5
msubhansp <m.subhan@synergicprofessionals.com>
2021-09-16 22:13:35 +0500

    React redux typescript start template initialized

---
+++ React-Redux-Typescript-starter-template/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Congratulations! Your App is live. Start working.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ 
