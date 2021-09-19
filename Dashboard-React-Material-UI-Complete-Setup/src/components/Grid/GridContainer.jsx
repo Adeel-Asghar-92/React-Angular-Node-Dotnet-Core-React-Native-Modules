3f0057ef6c30edbe4a41e56a9444f3950a50607c
msubhansp <m.subhan@synergicprofessionals.com>
2021-09-19 22:13:42 +0500

    Initialized Material UI Dashboard for React & Angular

---
+++ Dashboard-React-Material-UI-Complete-Setup/src/components/Grid/GridContainer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+
+const style = {
+  grid: {
+    margin: "0 -15px",
+    width: "calc(100% + 30px)"
+    // '&:before,&:after':{
+    //   display: 'table',
+    //   content: '" "',
+    // },
+    // '&:after':{
+    //   clear: 'both',
+    // }
+  }
+};
+
+function GridContainer({ ...props }) {
+  const { classes, children, className, ...rest } = props;
+  return (
+    <Grid container {...rest} className={classes.grid + " " + className}>
+      {children}
+    </Grid>
+  );
+}
+
+GridContainer.propTypes = {
+  classes: PropTypes.object,
+  className: PropTypes.string,
+  children: PropTypes.node
+};
+
+export default withStyles(style)(GridContainer);
---
+++ Dashboard-React-Material-UI-Complete-Setup/src/components/Grid/GridItem.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+
+const style = {
+  grid: {
+    padding: "0 15px !important"
+  }
+};
+
+function GridItem({ ...props }) {
+  const { classes, children, className, ...rest } = props;
+  return (
+    <Grid item {...rest} className={classes.grid + " " + className}>
+      {children}
+    </Grid>
+  );
+}
+
+GridItem.propTypes = {
+  classes: PropTypes.object,
+  className: PropTypes.string,
+  children: PropTypes.node
+};
+
+export default withStyles(style)(GridItem);
